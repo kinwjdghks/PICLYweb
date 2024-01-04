09c8b9ba5a99dc38cafe07b87540415941f01e8a
JungHwan <wjdghksdl00@gmail.com>
2024-01-04 15:09:10 +0900

    overscroll test

--- pico/components/page/PageFrame.tsx
+++ pico/components/page/PageFrame.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const PageFrame = ({children,className}:{children:ReactNode,className:string}):ReactNode =>{
-    return <div className={`lg:h-screen h-[100svh] overscroll-y-none ${className}}`}>{children}</div>
+    return <div className={`lg:h-screen h-[100svh] overscroll-y-contain ${className}}`}>{children}</div>
 }
 
 export default PageFrame;
