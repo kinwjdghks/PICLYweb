70c15146531e3c1d1af738979b61a0fa1e07d648
JungHwan <wjdghksdl00@gmail.com>
2024-01-11 02:09:23 +0900

    fallback page fixed

--- pico/components/page/ExpiredPage.tsx
+++ pico/components/page/ExpiredPage.tsx
@@ -1,7 +1,17 @@
+import { poppins } from "@/public/assets/fonts/poppins";
+import Link from "next/link";
+
 const ExpiredPage = () =>{
 
     return <div className="w-screen h-screen bg-black items-center relative">
-        <p className="w-full mt-[25%] text-center text-2xl">기한이 만료된 데이터입니다</p>
+        <div  className={`(actionbar) w-screen h-max fixed flex items-center top-0 lg:p-12 p-4 pt-2 ${poppins.className} z=[102]`}>
+        <Link className="w-max h-max font-bold lg:text-4xl text-2xl"
+          href={"/"}
+          onClick={() => {}}>
+          PiCo
+        </Link>
+      </div>
+        <p className="w-full mt-[50%] text-center text-2xl">기한이 만료된 데이터입니다</p>
     </div>
 }
 
--- pico/components/page/FallbackPage.tsx
+++ pico/components/page/FallbackPage.tsx
@@ -1,10 +1,22 @@
+import { poppins } from "@/public/assets/fonts/poppins";
+import Link from "next/link";
 import { ReactNode } from "react";
 
-const FallbackPage = ():ReactNode => {
-
-    return <div className="w-screen h-screen bg-black items-center relative">
-        <p className="w-full mt-[25%] text-center text-2xl">데이터를 찾을 수 없습니다</p>
+const FallbackPage = (): ReactNode => {
+  return (
+    <div className="w-screen h-screen bg-black items-center relative">
+      <div  className={`(actionbar) w-screen h-max fixed flex items-center top-0 lg:p-12 p-4 pt-2 ${poppins.className} z=[102]`}>
+        <Link className="w-max h-max font-bold lg:text-4xl text-2xl"
+          href={"/"}
+          onClick={() => {}}>
+          PiCo
+        </Link>
+      </div>
+      <p className="w-full mt-[50%] text-center text-2xl">
+        데이터를 찾을 수 없습니다
+      </p>
     </div>
-}
+  );
+};
 
 export default FallbackPage;
