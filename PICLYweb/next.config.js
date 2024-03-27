a5332e89162ea7bf820e0d76e97fd655aa4e0887
JungHwan <kinwjdghks@naver.com>
2024-03-28 01:20:58 +0900

    placeholder blur test

--- PICLYweb/next.config.js
+++ PICLYweb/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    formats:['image/avif', 'image/webp'],
     domains: ["firebasestorage.googleapis.com"],
     minimumCacheTTL: 31536000,
   },
